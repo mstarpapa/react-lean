8581d9a77c3105fcfacd2770a25e25136bcc695a
choi jaehong <myjaehong@gmail.com>
2020-04-25 02:13:40 +0900

    mac book 작업내용 갱신

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
-import React ,{useRef, useState} from 'react';
+import React ,{useRef, useState, useMemo, useCallback} from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
 
+  return users.filter(user => user.active).length;
+}
 function App() {
   const [inputs, setInputs] = useState({
     username: '',
     email:'',
   });
   const {username, email} = inputs;
-  const onChange = e => {
+  const onChange = useCallback(e => {
     const {name, value} = e.target;
     setInputs({
       ...inputs,
       [name] : value
     });
-  };
+  },[inputs]);
   const [users, setUsers] = useState([
     {
         id:1,
         username:'velopert',
-        email:'public.velopert@gmail.com'    
+        email:'public.velopert@gmail.com',
+        active: true,    
     },
     {
         id:2,
         username:'tester',
-        email:'tester@example.com'
+        email:'tester@example.com',
+        active:false,
     },
     {
          id:3,
          username:'liz',
-         email:'liz@example.com'
+         email:'liz@example.com',
+         active:false,
     }
   ]);
 
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id:nextId.current,
       username,
@@ -49,15 +56,30 @@ function App() {
       email:''
     });
     nextId.current +=1;
-  };
+  },[username, email, users]);
 
-  const onRemove = id => {
+  const onRemove = useCallback(id => {
     setUsers(users.filter(user => user.id !== id));
-  };
+  },[users]);
+
+  const onToggle = useCallback(id => {
+    setUsers(users.map(
+      user => user.id ===id
+      ? {...user, active: !user.active}
+      : user 
+    ));
+  },[users]);
+  const count = useMemo(() => countActiveUsers(users),[users]);
   return ( 
     <>
-    <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
-    <UserList users={users} onRemove={onRemove}/>
+    <CreateUser 
+      username={username} 
+      email={email} 
+      onChange={onChange} 
+      onCreate={onCreate}
+    />
+    <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+    <div>활성 사용자 수: {count}</div>
     </>
   );   
   
