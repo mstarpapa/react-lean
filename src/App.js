e6286991b79e20a045cfedb276af844524a5f20d
choi jaehong <myjaehong@gmail.com>
2020-04-27 01:53:30 +0900

    CreateUser 컴포넌트에서도 dispatch 를 직접 하도록 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React ,{useRef, useReducer, useMemo, useCallback, createContext} from 'react';
+import React ,{useReducer, useMemo} from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
-import useInputs from './useInputs';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
-
   return users.filter(user => user.active).length;
 }
 const initialState = {
@@ -54,41 +52,15 @@ function reducer(state, action) {
       throw new Error('Unhandled action');
   }
 }
-export const UserDispatch = createContext(null);
+export const UserDispatch = React.createContext(null);
 function App() {
   const [ state, dispatch] = useReducer(reducer, initialState);
-  const [form, onChange, reset] = useInputs({
-    username:'',
-    email:'',
-  });
-  const {username, email} = form;
-  const nextId = useRef(4);
   const {users} = state;
-
-  const onCreate = useCallback(() => {
-    dispatch({
-      type:'CREATE_USER',
-      user:{
-        id:nextId.current,
-        username,
-        email,
-      }
-    });
-    nextId.current +=1;
-    reset();
-  },[username, email, reset])
-
   
   const count = useMemo(() => countActiveUsers(users),[users]);
   return ( 
     <UserDispatch.Provider value={dispatch}>
-    <CreateUser 
-      username={username} 
-      email={email} 
-      onChange={onChange}
-      onCreate={onCreate}
-
-    />
+    <CreateUser/>
     <UserList users={users}/>
     <div>활성 사용자 수: {count}</div>
     </UserDispatch.Provider>
